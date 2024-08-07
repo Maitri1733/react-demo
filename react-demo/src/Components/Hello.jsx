aefeb3f81fcbc4149ca918f2b74f85e02b182ee5
maitri <machauhan1733@gmail.com>
2024-08-07 23:26:08 +0530

    Change h1 to h2

--- react-demo/src/Components/Hello.jsx
+++ react-demo/src/Components/Hello.jsx
@@ -7,9 +7,9 @@
 function Hello({ person }) {
   return (
     <div>
-      <h1>
+      <h2>
         {person.message} {person.emoji} {person.name} {person.seatNumbers}
-      </h1>
+      </h2>
       {/* <h1>Hello there is components!</h1>
       <h1>{DisplayMessage()}</h1>
       <h1>{10 + 20}</h1>
